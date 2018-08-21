d171b8f4faa01558e2ed46993995a9a20903ac73
chennill <huangchen@yonghui.cn>
2018-08-21 22:15:53 +0800

    modify files

--- source/js/main.js
+++ source/js/main.js
@@ -200,5 +200,11 @@ require([], function (){
             }
         }
     })
+    
 
 })
+  if($(".instagram").length) {
+        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+            obj.init();
+        });
+    }
